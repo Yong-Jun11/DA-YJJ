cf9bea73e121ebec72879335f81642694ba5abe7
Yong-Jun11 <kwek_yong_jun@connect.ite.edu.sg>
2020-09-11 16:57:00 +0800

    project & testcase

--- project.py
+++ project.py
@@ -2,26 +2,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_excel("IMVA.xlsx")
-print(df)
+#print(df)
 
-df1 = df[["Periods", " United Kingdom ", " Germany ", " France ", " Italy ", " Netherlands ", " Greece ", " Belgium & Luxembourg "," Switzerland ",	" Austria ", " Scandinavia "]]
+df1 = df[["Periods", " United Kingdom ", " Germany ", " France ", " Italy ",
+          " Netherlands ", " Greece ", " Belgium & Luxembourg "," Switzerland ", " Austria ", " Scandinavia "]]
 
 Countries = df1['Periods'].str.split(' ', n = 2, expand = True)
-print(Countries)
+#print(Countries)
 
 df1 = df1.assign(year=Countries[1])
 df1 = df1.assign(month=Countries[2])
 df1["year"] = pd.to_numeric(df1["year"])
-print(df1)
+#print(df1)
 
 df3 = df1[(df1["year"] >= 1988) & (df1["year"] <= 1997)]
-print(df3)
+#print(df3)
 
-df4 = df3[[" Germany ", " France ", " Italy ", " Netherlands ", " Greece ", " Belgium & Luxembourg "," Switzerland ",	" Austria ", " Scandinavia "]]
-print(df4)
+df4 = df3[[" Germany ", " France ", " Italy ", " Netherlands ", " Greece ",
+           " Belgium & Luxembourg "," Switzerland ",	" Austria ", " Scandinavia "]]
+#print(df4)
 ps = df4.sum().sort_values(ascending=False)
 top3countries = ps.head(3)
 top3countries.index
+total = top3countries.sum()
+mean = round(top3countries.mean(),2)
+print("total is ", total)
+print("mean is", mean)
+
 
 import numpy as np
 index = np.arange(len(top3countries.index))
@@ -31,7 +38,7 @@ plt.ylabel('No. of Travellers (in thousands)', fontsize=8)
 plt.xticks(index, top3countries.index, fontsize=7, rotation=60)
 plt.title('Top 3 Europe Countries from 1988-1997')
 plt.bar(top3countries.index, top3countries.values/1000)
-plt.show();
+#plt.show();
 
 index = np.arange(len(ps.index))
 plt.xlabel('Countries (Others)', fontsize=10)
@@ -39,7 +46,7 @@ plt.ylabel('No. of Travellers (in thousands)', fontsize=8)
 plt.xticks(index, ps.index, fontsize=7, rotation=60)
 plt.title('Total Europe Countries from 1988-1997')
 plt.bar(ps.index, ps.values/1000)
-plt.show();
+#plt.show();
 
 
 
--- testcase.py
+++ testcase.py
@@ -1,9 +1,12 @@
 import unittest
+import project as prog
 
 class TestMyProgram(unittest.TestCase):
+    def test_Total(self):
+        self.assertEqual(prog.total, 3192865)
 
-    def test_EngineType(self):
-        print("Testing")
+    def test_Mean(self):
+        self.assertEqual(prog.mean, 1064288.34)
 
 if __name__ == '__main__':
     unittest.main()
